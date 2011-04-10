6c3b5e884678f61727694e1524d822ded85118c7
Samus_ <mail2samus@gmail.com>
2011-04-10 19:57:44 -0300

    adding node listing

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ class Node(db.Model):
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        node_string = ','.join("\"%s\"" % node.title.replace("\\", "\\\\").replace("\"", "\\\"") for node in Node.all())
+        nodes = Node.all()
+        node_string = ','.join("\"%s\"" % node.title.replace("\\", "\\\\").replace("\"", "\\\"") for node in nodes)
 
         head = """<title>Tamarindo</title>"""
-        body = """<form action="." method="post"><input type="text" name="title"><input type="submit" value="Add"></form><img src="http://chart.googleapis.com/chart?cht=gv&amp;chl=digraph{%s}">""" % urllib.quote(node_string)
+        lcol = """<form action="." method="post"><input type="text" name="title"><input type="submit" value="Add"></form>""" + \
+               """<ul>%s</ul>""" % ''.join("<li>%s</li>" % node.title for node in nodes)
+        rcol = """<img src="http://chart.googleapis.com/chart?cht=gv&amp;chl=digraph{%s}">""" % urllib.quote(node_string)
+        body = """<table><tr><td>%s</td><td>%s</td></tr></table>""" % (lcol, rcol)
         page = """<html><head>%s</head><body>%s</body></html>""" % (head, body)
 
         self.response.out.write(page)
