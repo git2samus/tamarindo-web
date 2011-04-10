a942de6cb189251e8d259a9b13c1de6ada8535e2
Samus_ <mail2samus@gmail.com>
2011-04-10 19:33:45 -0300

    adding google-charts digraph representation of nodes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import cgi
+import cgi, urllib
 
 from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp import util
@@ -10,15 +10,19 @@ class Node(db.Model):
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
+        node_string = ','.join("\"%s\"" % node.title.replace("\\", "\\\\").replace("\"", "\\\"") for node in Node.all())
+
         head = """<title>Tamarindo</title>"""
-        body = """<form action="." method="post"><input type="text" name="title"><input type="submit" value="Add"></form>"""
+        body = """<form action="." method="post"><input type="text" name="title"><input type="submit" value="Add"></form><img src="http://chart.googleapis.com/chart?cht=gv&amp;chl=digraph{%s}">""" % urllib.quote(node_string)
         page = """<html><head>%s</head><body>%s</body></html>""" % (head, body)
+
         self.response.out.write(page)
 
     def post(self):
         node = Node()
         node.title = cgi.escape(self.request.get('title'))
         node.put()
+
         self.redirect('/')
 
 
