6f9a46dfb7ffabebc0d0ceb912449e18fd246953
Samus_ <mail2samus@gmail.com>
2011-04-11 03:28:52 -0300

    refactor

--- main.py
+++ main.py
@@ -8,34 +8,39 @@ class Node(db.Model):
     title = db.StringProperty()
     associations = db.ListProperty(db.Key)
 
+    @property
+    def quoted_title(self):
+        return "\"%s\"" % self.title.replace("\\", "\\\\").replace("\"", "\\\"")
 
-def chart_url(nodes):
-    nodes_dict = dict((node.key(), node) for node in nodes)
-
-    title = lambda node: "\"%s\"" % node.title.replace("\\", "\\\\").replace("\"", "\\\"")
-    assoc = lambda node: ','.join("%s->%s" % (title(node), title(nodes_dict[child_key])) for child_key in node.associations)
-    digraph = ','.join(assoc(node) if node.associations else title(node) for node in nodes)
-    return "http://chart.googleapis.com/chart?cht=gv&amp;chl=digraph{%s}" % urllib.quote(digraph)
+    def digraph(self, nodes_dict=None):
+        if self.associations:
+            nodes_dict = nodes_dict or dict((node.key(), node) for node in Node.get(self.associations))
+            return ','.join("%s->%s" % (self.quoted_title, nodes_dict[child_key].quoted_title) for child_key in self.associations)
+        else:
+            return self.quoted_title
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         nodes = Node.all()
-        sorted_nodes = sorted(nodes, key=lambda node: node.title)
+        nodes.order('title')
+
+        nodes_dict = dict((node.key(), node) for node in nodes)
+        digraph = ','.join(node.digraph(nodes_dict) for node in nodes)
 
         head = """<title>Tamarindo</title>"""
         lcol = """<form action="." method="post"><input type="text" name="title"><input type="submit" value="Add"></form>""" + \
                """<form action="." method="post">%s -> %s <input type="submit" value="Connect"></form>""" % (
             """<select name="assoc_from"><option>---</option>%s</select>""" % ''.join(
-                """<option value="%d">%s</option>""" % (node.key().id(), node.title) for node in sorted_nodes
+                """<option value="%d">%s</option>""" % (node.key().id(), node.title) for node in nodes
             ),
             """<select name="assoc_to"><option>---</option>%s</select>""" % ''.join(
-                """<option value="%d">%s</option>""" % (node.key().id(), node.title) for node in sorted_nodes
+                """<option value="%d">%s</option>""" % (node.key().id(), node.title) for node in nodes
             ),
         ) + \
                """<ul>%s</ul>""" % ''.join(
-            """<li><a href="#%d">%s</a></li>""" % (node.key().id(), node.title) for node in sorted_nodes
+            """<li><a href="#%d">%s</a></li>""" % (node.key().id(), node.title) for node in nodes
         )
-        rcol = """<img src="%s">""" % chart_url(nodes)
+        rcol = """<img src="http://chart.googleapis.com/chart?cht=gv&amp;chl=digraph{%s}">""" % urllib.quote(digraph)
         body = """<table><tr><td>%s</td><td>%s</td></tr></table>""" % (lcol, rcol)
         page = """<html><head>%s</head><body>%s</body></html>""" % (head, body)
 
