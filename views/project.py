95bbae39854334997589ab6097580a1ed97faa50
Samus_ <mail2samus@gmail.com>
2011-04-22 04:53:49 -0300

    preventing users from accesing each others projects

--- views/project.py
+++ views/project.py
@@ -12,42 +12,48 @@ class ProjectHandler(RequestHandler):
         project = Project.get_by_id(long(project_id))
         if project:
             user = users.get_current_user()
+            if project.owner == user.user_id():
+                nodes = Node.all()
+                nodes.ancestor(project)
+                nodes.order('title')
+                nodes = tuple(nodes) # prevent re-execution when iterating
 
-            nodes = Node.all()
-            nodes.ancestor(project)
-            nodes.order('title')
-            nodes = tuple(nodes) # prevent re-execution when iterating
+                nodes_dict = dict((node.key(), node) for node in nodes)
+                digraph = "digraph{%s}" % ';'.join(node.digraph(nodes_dict) for node in nodes)
+                chart_url = "http://chart.googleapis.com/chart?cht=gv&chl=%s" % urllib.quote(digraph)
 
-            nodes_dict = dict((node.key(), node) for node in nodes)
-            digraph = "digraph{%s}" % ';'.join(node.digraph(nodes_dict) for node in nodes)
-            chart_url = "http://chart.googleapis.com/chart?cht=gv&chl=%s" % urllib.quote(digraph)
+                context = context_dict(locals(), 'user', 'nodes', 'digraph', 'chart_url')
+                context['logout_url'] = users.create_logout_url('/')
 
-            context = context_dict(locals(), 'user', 'nodes', 'digraph', 'chart_url')
-            context['logout_url'] = users.create_logout_url('/')
-
-            page = template.render('templates/project.html', context)
-            self.response.out.write(page)
+                page = template.render('templates/project.html', context)
+                self.response.out.write(page)
+            else:
+                self.error(403)
         else:
             self.redirect('/')
 
     def post(self, project_id):
         project = Project.get_by_id(long(project_id))
         if project:
-            title = self.request.get('title')
-            assoc_from, assoc_to = self.request.get('assoc_from'), self.request.get('assoc_to')
+            user = users.get_current_user()
+            if project.owner == user.user_id():
+                title = self.request.get('title')
+                assoc_from, assoc_to = self.request.get('assoc_from'), self.request.get('assoc_to')
 
-            if title:
-                Node(
-                    parent=project,
-                    title=title,
-                ).put()
-            elif assoc_from and assoc_to and assoc_from.isdigit() and assoc_to.isdigit():
-                node_from, node_to = Node.get_by_id(long(assoc_from)), Node.get_by_id(long(assoc_to))
-                if node_from and node_to:
-                    node_from.associations.append(node_to.key())
-                    node_from.put()
+                if title:
+                    Node(
+                        parent=project,
+                        title=title,
+                    ).put()
+                elif assoc_from and assoc_to and assoc_from.isdigit() and assoc_to.isdigit():
+                    node_from, node_to = Node.get_by_id(long(assoc_from)), Node.get_by_id(long(assoc_to))
+                    if node_from and node_to:
+                        node_from.associations.append(node_to.key())
+                        node_from.put()
 
-            self.redirect("/%s/" % project_id)
+                self.redirect("/%s/" % project_id)
+            else:
+                self.error(403)
         else:
             self.redirect('/')
 
