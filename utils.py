666e83a21cdeb96f1823d0071e878b81646b52a8
Samus_ <mail2samus@gmail.com>
2011-04-22 06:17:08 -0300

    allow undefined values on context_dict

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
 def context_dict(source_dict, *varnames):
     # helper to initialize template context from locals()
-    return dict((varname, source_dict[varname]) for varname in varnames)
+    return dict((varname, source_dict.get(varname)) for varname in varnames)
