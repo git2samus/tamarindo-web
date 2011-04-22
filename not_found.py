3cd4e3487446f67781c9ef24d712ce3c65f9e659
Samus_ <mail2samus@gmail.com>
2011-04-21 22:50:17 -0300

    adding separate CGI script to handle 404 errors

---
+++ not_found.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def main():
+    print 'Status: 404 Not Found'
+    print 'Content-Type: text/plain'
+    print
+    print 'Not Found'
+
+
+if __name__ == '__main__':
+    main()
+
